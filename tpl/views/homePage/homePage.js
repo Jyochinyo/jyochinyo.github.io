71cbc65331c45105784af62cbd60ee33e18bae9a
jyo <465284352@qq.com>
2018-06-21 14:53:33 +0800

    t

---
+++ tpl/views/homePage/homePage.js
@@ -0,0 +1,35 @@
+app.controller('homePageController', function($rootScope, $scope, $http, $timeout, customService) {
+    
+    
+
+});
+
+
+
+app.controller('HeaderController', function ($rootScope, $scope, $http, $timeout, customService) {
+    $scope.titleClick = function () {
+        alert(123);
+    };
+});
+
+app.controller('SidebarController', function ($rootScope, $scope, $http, $timeout,$state, customService) {
+    
+    $scope.goPageClick = function (tag) {
+        if (tag == 1) {
+            $state.go('homePage.firstPage');
+        } else if (tag == 2) {
+            $state.go('homePage.secondPage');
+        } else if (tag == 3) {
+            $state.go('homePage.filter');
+        } else if (tag == 4) {
+            $state.go('homePage.formCheck');
+        } else if (tag == 5) {
+            $state.go('homePage.directive');
+        } else if (tag == 6) {
+            $state.go('homePage.service');
+        } else if (tag == 7) {
+            $state.go('homePage.playGround');
+        }
+        
+    };
+});
