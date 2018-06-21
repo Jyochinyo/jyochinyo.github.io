d0d611fd47f4b713c46886a51bd043932660fa8b
jyo <465284352@qq.com>
2018-06-21 15:01:18 +0800

    t

--- assets/js/customFilter.js
+++
@@ -1,9 +0,0 @@
-app
-    .filter('capitalize', function () {
-        // input是我们传入的字符串
-        return function (input) {
-            if (input) {
-                return input[0].toUpperCase() + input.slice(1);
-            }
-        }
-    });
